19e54922b253a8cd37ec1e91e49ff7925b5656b6
Hichxm <hicham.slimani.fr@gmail.com>
2018-11-06 21:48:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const argv = minimist(process.argv.slice(2))
 // In a custom directory or current directory
 const targetPath = path.resolve(argv._[0] || '.')
 
+console.log(targetPath);
+
 console.log(`> Generating Discord.json project in ${targetPath}`)
 
 sao({
